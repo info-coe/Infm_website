8a2d9785c631cb021899ca98d256275e27efa5f7
info-coe <coeteam@infomericainc.in>
2024-05-22 19:02:39 +0530

    latest commit

--- frontend/src/reusablecomponents/CrmServices.js
+++ frontend/src/reusablecomponents/CrmServices.js
@@ -7,7 +7,7 @@ const CrmServices = ({ serviceData }) => {
         <div key={index} className="row mb-4">
           <div className="col-md-12">
             <h3 className='text-primary mb-4'>{service.mainHead}</h3>
-            <h4 className='text-success'>{service.subHead}</h4>
+            <h5 className='text-danger'>{service.subHead}</h5>
             <p className='fs-5 text-secondary'><strong>Objective: </strong>{service.objective}</p>
             {service.approach ? (
               <p className='fs-5 text-secondary'>
--- frontend/src/reusablecomponents/Zoomin2.js
+++ frontend/src/reusablecomponents/Zoomin2.js
@@ -9,10 +9,10 @@ const Zoomin2 = (props) => {
         <>
 
         
-        <div data-aos="zoom-in" style={{fontSize:'20px'}}>
+        <div data-aos="zoom-in" style={{fontSize:' 17px'}}>
             <h2 className='text-secondary'style={{fontFamily: "'Lobster', cursive"}}>{props.Mainheading}</h2>
             <h1 className='text-primary'>{props.heading}</h1>
-            <h1 className='text-success'>{props.heading2}</h1>
+            <h1 className='text-danger'>{props.heading2}</h1>
             <div>
                 {props.content.map((item,index)=>(
                     <p key={index}><strong>{props.s} </strong>{item}</p>
