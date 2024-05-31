682eeeccab7c604dfefefe4d39f52476d6532acd
infocoe <coeteam@infomericainc.in>
2024-05-31 17:44:56 +0530

    contactus page

--- frontend/src/maincomponents/CONTACTUS/contactus.js
+++ frontend/src/maincomponents/CONTACTUS/contactus.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import ContactusContent from "./contactusContent.json"
+import CorporateofficeaddressCard from '../../reusablecomponents/CorporateofficeaddressCard';
+
 
 const Contactus = () => {
+    const data=ContactusContent
+    
+  
     return (
         <div>
-            <h1>Contactus</h1>
+                 <img
+        src={data[0].contactusbanner}
+        width="100%"
+        alt="Main Service"
+        className="mainimg img-fluid d-none d-md-block"
+        style={{ objectFit: "cover" }}
+      />
+      <img
+        src={data[0].contactusMobilebanner}
+        alt="Sub Service"
+        className="sunimg img-fluid d-md-none w-100"
+        style={{ objectFit: "cover" }}
+      />
+        <div className='container text-center pt-md-5 pb-md-5 p-3'>
+                <p className='text-center ps-md-5 pe-md-5 ms-md-5 me-md-5 mb-5' style={{fontSize:"19px"}}>{data[0].description}</p>
+                
+                <div className='text-white p-lg-5 p-3 ms-lg-5 me-lg-5' style={{backgroundImage: `url(${data[0].contactform})`,objectFit:"cover", backgroundRepeat:"no-repeat", backgroundSize:"cover", borderRadius:'20px'}}>
+                    <h2>{data[0].contactusHead}</h2>
+                    <form>
+                    <div className='d-md-flex m-1'>
+                  <input type="text" placeholder='First Name' className='form-control m-md-2 mt-2 mb-2 rounded-pill p-3' required/>
+                  <input type="text" placeholder='Last Name' className='form-control  m-md-2 mt-2 mb-2 rounded-pill p-3' required/>
+                  </div>
+                  <div className='d-md-flex m-1'>
+                    <input type="number" placeholder='Mobile Number' className='form-control  m-md-2 mt-2 mb-2 rounded-pill p-3' required/>
+                    <input type="email" placeholder='Email' className='form-control  m-md-2 mt-2 mb-2 rounded-pill p-3'required/>
+                  </div>
+                   <div className='m-3'>
+                   <textarea placeholder='Message' className='form-control rounded' style={{height:"150px"}}></textarea>
+                   </div>
+                   <div>
+                    <button className='btn btn-danger m-2'>Submit</button>
+                  
+                   </div>
+                    </form>
+                    
+                </div>
+                <div className='mt-5 mb-5'>
+                    <h2 className='pt-5'><span className='text-primary'>{data[0].CorporateOffice}</span>&nbsp;<span className='text-danger'>{data[0].Address}</span></h2>
+                    <div className='d-lg-flex'>
+                    <CorporateofficeaddressCard data={data[1]}/>
+                    </div>
+                </div>
+                
+        </div>            
         </div>
     );
 };
---
+++ frontend/src/reusablecomponents/CorporateofficeaddressCard.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+const CorporateofficeaddressCard = ({data}) => {
+    console.log(data.address)
+    const address=data.address
+    const styles = `
+    .card {
+        position: relative;
+        width: 340px;
+        height: 300px;
+        background-color: #FFFFFF;
+        border-radius: 20px;
+        overflow: hidden;
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+        transition: transform 0.3s, background-color 0.3s, color 0.3s;
+        transform: scale(1);
+        transition: 1s;
+        margin: 30px auto; 
+    }
+    .content {
+        padding: 20px;
+        position: relative;
+        z-index: 2;
+        margin: 10px;
+        border-radius: 10px;
+        height: auto;
+        min-height: 190px;
+    }
+    .constoverlay {
+        position: absolute;
+        top: 12%;
+        right: -40%;
+        bottom: 30%;
+        width: 75%;
+        height: 98%;
+        background-color: #e6e6fa;
+        border-radius: 10%;
+        z-index: 0;
+        rotate: 30deg;
+    }
+    .overlay {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        width: 100px;
+        height: 100px;
+        background-color: #D3D3D3;
+        border-radius: 10%;
+        transition: all 0.3s ease;
+        clip-path: circle(50% at 100% 100%);
+        z-index: 1;
+    }
+    .card:hover .overlay {
+        width: 120%;
+        height: 100%;
+        clip-path: circle(150% at 150% 150%);
+        border-radius: 0%;
+        background-color: #0066b2;
+        opacity: 0.9;
+    }
+    .card:hover {
+        background-color: #007FFF;
+        color: white;
+        transform: scale(1.1);
+        z-index: 10;
+        transition: 1s;
+    }
+   
+    @media screen and (max-width: 767px){
+        .card{
+            width:290px;
+        }
+    }
+    @media screen and (min-width: 768px) and (max-width:991px){
+        .card{
+            width:100%;
+        }
+    }
+    
+    }
+    `;
+    return (
+        <>
+         <style>{styles}</style>
+         {
+            address.map((item,index)=>(
+                <div className="card col-md-4">
+                    <div className="content text-center">
+                        <h4 className='mb-4'>{item.name}</h4>
+                        <p><i className={item.locationicon}></i>&nbsp;{item.addr}</p>
+                        <p><i className={item.emailicon}></i>&nbsp;{item.email}</p>
+                        <h4><i className={item.phoneicon}></i>&nbsp;{item.phone}</h4>
+
+                    </div>
+                    <div className="constoverlay"></div>
+                    <div className="overlay"></div>
+                </div>
+            ))
+         }
+              
+        </>
+    );
+};
+
+export default CorporateofficeaddressCard;
