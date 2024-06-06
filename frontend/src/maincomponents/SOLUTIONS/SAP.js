f70ddcaf14b2b40b56a207792738a1e93edca798
info-coe <coeteam@infomericainc.in>
2024-06-06 18:17:25 +0530

    solutions SAP

--- frontend/src/maincomponents/SOLUTIONS/SAP.js
+++ frontend/src/maincomponents/SOLUTIONS/SAP.js
@@ -2,6 +2,7 @@ import React from "react";
 import SolutionData from "./solutionscontent.json";
 import ModelsPoints from "../../reusablecomponents/ModelsPoints";
 import AboutZoomin from "../../reusablecomponents/AboutZoomin";
+import ToggleableCard from "../../reusablecomponents/ToggleableCard";
 
 const SAP = (props) => {
   const serviceContent = JSON.parse(props.product.ServicesComponent.Content);
@@ -23,7 +24,31 @@ const SAP = (props) => {
         />
       </div>
       <div className="text-center m-5">
-        <img src="https://www.infomericainc.com/Content/images/sap.png" alt="SAPMAP"/>
+        <img src={SAPData.SAP_Portfolio_Image} alt="SAPMAP" />
+      </div>
+      <div className="container d-md-flex gap-4">
+        <div className="col-md-6">
+          <h4 className="text-danger mb-4">{SAPData.SAP_Services_Portfolio_Heading}</h4>
+          {SAPData.SAP_Services_Portfolio.map((item, index) => (
+            <ToggleableCard
+              key={index}
+              icon={item.icon}
+              title={item.title}
+              content={item.content}
+            />
+          ))}
+        </div>
+        <div className="col-md-6">
+          <h4 className="text-danger mb-4">{SAPData.SAP_Focus_Offering_Heading}</h4>
+          {SAPData.SAP_Focus_Offering.map((item, index) => (
+            <ToggleableCard
+              key={index}
+              icon={item.icon}
+              title={item.title}
+              content={item.content}
+            />
+          ))}
+        </div>
       </div>
       <ModelsPoints contents={crmModelPointsData} />
     </div>
