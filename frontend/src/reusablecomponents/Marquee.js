400e6b7a4b5d6224c106db97b78d126489473f01
infocoe <coeteam@infomericainc.in>
2024-09-26 12:57:28 +0530

    updates

--- frontend/src/reusablecomponents/Marquee.js
+++ frontend/src/reusablecomponents/Marquee.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Marquee = ({ content }) => {
   return (
-    <div className="Marquee">
+    <div className="Marquee mt-4">
       <div className="Marquee-content">
         <div className="Marquee-tag">{ content }</div>
       </div>
